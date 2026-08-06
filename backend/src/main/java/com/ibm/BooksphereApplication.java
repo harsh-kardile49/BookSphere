165081969a2370224b7dc2e42ea915c41b2f33e2
Saumajit Malakar <saumajit@li-4c4c4544-0048-5010-8050-b6c04f4e4a34.ibm.com>
2026-08-06 14:33:30 +0530

    Added Spring Boot backend scaffold

---
+++ backend/src/main/java/com/ibm/BooksphereApplication.java
@@ -0,0 +1,13 @@
+package com.ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksphereApplication.class, args);
+	}
+
+}
